a6b9a8cce288a6bab984444c6c8734f0d4f6d310
SHAURYAGUPTA01 <86559522+SHAURYAGUPTA01@users.noreply.github.com>
2022-01-23 12:35:39 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBoG0KH3OHb9AhxSMw-TDVI-RRmYfDTvY4",
+    authDomain: "story-telling-e6a47.firebaseapp.com",
+    databaseURL: "https://story-telling-e6a47-default-rtdb.firebaseio.com",
+    projectId: "story-telling-e6a47",
+    storageBucket: "story-telling-e6a47.appspot.com",
+    messagingSenderId: "1057660871116",
+    appId: "1:1057660871116:web:52f9c1573d0347c502fd69"
+  };
